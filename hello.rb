6b0ea11d10acafd74d3074674ec5064130376b05
Ben <ben.c.hawker@gmail.com>
2015-08-03 12:40:50 +0100

    Kittens

--- hello.rb
+++ hello.rb
@@ -1,5 +1,12 @@
-require 'sinatra'
+require "sinatra"
+require "shotgun"
 
 get '/'  do
-	'hello!'
+	"<h1>Hello, this a test Sinatra application - we are riding Shotgun</h1><div>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
+
+get '/secret' do
+	"This is a secret page"
 end
